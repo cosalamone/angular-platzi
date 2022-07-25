b1461a0230bc74aa09f41807e135814d82ff7a01
cosalamone <cosalamone@gmail.com>
2022-07-25 20:52:37 -0300

    class and styles

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -8,6 +8,7 @@ import { Product } from './product.model';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
+  widthImg=10
   name = 'Constanza';
   age = 18;
   img = "https://t2.ev.ltmcdn.com/es/posts/4/8/4/plantar_suculentas_como_hacerlo_2484_orig.jpg";
