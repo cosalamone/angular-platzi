f391d037e7a1fc371477df57390eeebe36fa7280
cosalamone <cosalamone@gmail.com>
2022-07-24 21:38:58 -0300

    String interpolation

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'my-store';
+  name = 'Constanza';
+  age = 18;
+  img = "https://t2.ev.ltmcdn.com/es/posts/4/8/4/plantar_suculentas_como_hacerlo_2484_orig.jpg";
 }
