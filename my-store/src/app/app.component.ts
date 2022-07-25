116330d8b6e178f9dc53515a3546e71d6e6f2867
cosalamone <cosalamone@gmail.com>
2022-07-25 17:49:46 -0300

    Otros eventos: Scroll, input

--- my-store/src/app/app.component.ts
+++ my-store/src/app/app.component.ts
@@ -10,18 +10,27 @@ export class AppComponent {
   age = 18;
   img = "https://t2.ev.ltmcdn.com/es/posts/4/8/4/plantar_suculentas_como_hacerlo_2484_orig.jpg";
   buttonDisable = true;
-  person ={
+  person = {
     name: "Nicolas",
     age: 40,
     avatar: "https://www.w3schools.com/howto/img_avatar.png"
   }
 
-  toggleButton(){
+  toggleButton() {
     this.buttonDisable = !this.buttonDisable;
   }
 
-  increaseAge(){
-    this.person.age +=1;
+  increaseAge() {
+    this.person.age += 1;
   }
 
+  onScroll(event: Event) {
+    const elemento = event.target as HTMLElement;
+    console.log(elemento.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const elemento = event.target as HTMLInputElement;
+    this.person.name = elemento.value;
+  }
 }
